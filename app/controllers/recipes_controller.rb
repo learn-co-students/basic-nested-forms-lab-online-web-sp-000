f8c1c39e851a5cee5ccfc52b3dbd730656b03dc5
Jonathan Tuzman <tuzmusic@gmail.com>
2018-12-25 12:14:34 +0100

    Done.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -2,15 +2,26 @@ class RecipesController < ApplicationController
   def show
     @recipe = Recipe.find(params[:id])
   end
-
+  
   def index
     @recipes = Recipe.all
   end
-
+  
   def new
-    @recipe = Recipe.new
+    @recipe = Recipe.new    
+    @recipe.ingredients.build(name:"")
+    @recipe.ingredients.build(name:"")
   end
 
   def create
+    @recipe = Recipe.create(recipe_params)
+    redirect_to recipes_path
+    # render :'recipes/new'
+  end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:title, ingredients_attributes:[:name, :quantity])
   end
 end
