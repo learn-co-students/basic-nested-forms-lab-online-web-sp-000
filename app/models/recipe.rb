e4befb4c34278942c7a9993b48c35bc8ca124c32
Jcg408 <jcg321@comcas.net>
2019-03-27 20:16:06 -0400

    Done.

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,9 @@
-class Recipe < ActiveRecord::Base
+class Recipe < ApplicationRecord
   has_many :ingredients 
+  accepts_nested_attributes_for :ingredients
+
+  def ingredient_attributes=(ingredient, quantity)
+    self.ingredient = ingredient.find_or_create_by(name: ingredient.name, quantity: ingredient.quantity)
+    self.ingredient.update(ingredient)
+  end
 end
