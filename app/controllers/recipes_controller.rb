f1646d1cfa1e954a178db50de5dab134859e4e76
github username <github email address>
2019-04-03 10:30:13 -0400

    Done.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -12,5 +12,21 @@ class RecipesController < ApplicationController
   end
 
   def create
+    # raise params.inspect
+    @recipe = Recipe.new(recipe_params)
+
+    if @recipe.save
+      redirect_to @recipe
+    else
+      render :new
+    end
   end
+
+
+  private
+
+    def recipe_params
+      params.require(:recipe).permit(:title, :ingredients_attributes => [:ingredient_1, :ingredient_2])
+    end
+
 end
